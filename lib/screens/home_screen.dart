6a10e780d674c45e10fdf44fb397658bde9b6e68
Filippo Valli <filippovalli2003@gmail.com>
2022-01-10 17:26:05 +0100

    fix HomeScreen()

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:sn/pages/chatlist_page.dart';
+import 'package:sn/pages/profile_page.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true,
         title: const Text('SN'),
       ),
-      body: SafeArea(
-        child: Center(),
+      body: _index == 0
+          ? const ChatlistPage()
+          : _index == 1
+              ? const ProfilePage()
+              : const ProfilePage(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _index,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.send), label: 'Send'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onTap: (int index) => setState(() {
+          _index = index;
+        }),
       ),
     );
   }
