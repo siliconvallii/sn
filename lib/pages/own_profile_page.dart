35b240ee2361e090cf8961c2982957839aa846c7
Filippo Valli <filippovalli2003@gmail.com>
2022-01-11 11:26:26 +0100

    fix OwnProfilePage()

--- lib/pages/own_profile_page.dart
+++ lib/pages/own_profile_page.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:sn/providers/sign_up.dart';
 
 class OwnProfilePage extends StatelessWidget {
   const OwnProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.network(
+                user['profile_picture'],
+                scale: 5,
+              ),
+              Column(
+                children: [
+                  const Text('x amici'),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Modifica profilo'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Text(user['username']),
+          Text('Liceo Sarpi - ' + user['class'] + user['section']),
+          Text(user['bio']),
+        ],
+      ),
+    );
   }
 }
