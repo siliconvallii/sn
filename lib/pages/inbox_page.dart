86923c3d292395beb3f7323e1cbe760f85c1c328
Filippo Valli <filippovalli2003@gmail.com>
2022-01-12 15:46:55 +0100

    add scroll down to refresh in InboxPage()

--- lib/pages/inbox_page.dart
+++ lib/pages/inbox_page.dart
@@ -6,9 +6,14 @@ import 'package:sn/providers/sign_in.dart';
 import 'package:sn/screens/watch_image_screen.dart';
 import 'package:sn/utils/take_image.dart';
 
-class InboxPage extends StatelessWidget {
+class InboxPage extends StatefulWidget {
   const InboxPage({Key? key}) : super(key: key);
 
+  @override
+  State<InboxPage> createState() => _InboxPageState();
+}
+
+class _InboxPageState extends State<InboxPage> {
   @override
   Widget build(BuildContext context) {
     // instatiate reference of Realtime Database
@@ -17,99 +22,114 @@ class InboxPage extends StatelessWidget {
     return FutureBuilder(
       future: fetchInbox(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        }
-        if (snapshot.hasError) {
-          return const Text('Non hai messaggi!');
-        } else {
-          return ListView.builder(
-            itemCount: snapshot.data.length,
-            itemBuilder: (BuildContext context, int index) {
-              if (snapshot.data[index]['replied_at'] != null) {
-                return Row(
-                  children: [
-                    Image.network(
-                      snapshot.data![index]['sender']['profile_picture'],
-                      scale: 10,
+        return RefreshIndicator(
+          onRefresh: () async {
+            await fetchInbox();
+            setState(() {});
+          },
+          child: (snapshot.connectionState == ConnectionState.waiting)
+              ? ListView(
+                  children: const [CircularProgressIndicator()],
+                )
+              : (snapshot.hasError)
+                  ? ListView(
+                      children: const [Text('Non hai messaggi.')],
+                    )
+                  : ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (snapshot.data[index]['replied_at'] != null) {
+                          return Row(
+                            children: [
+                              Image.network(
+                                snapshot.data![index]['sender']
+                                    ['profile_picture'],
+                                scale: 10,
+                              ),
+                              Column(
+                                children: [
+                                  Text(snapshot.data![index]['sender']
+                                      ['username']),
+                                  Text(
+                                    'Hai risposto ' +
+                                        snapshot.data![index]['replied_at'],
+                                  ),
+                                ],
+                              ),
+                              const Icon(Icons.arrow_upward),
+                            ],
+                          );
+                        } else if (snapshot.data[index]['viewed']) {
+                          return InkWell(
+                            child: Row(
+                              children: [
+                                Image.network(
+                                  snapshot.data![index]['sender']
+                                      ['profile_picture'],
+                                  scale: 10,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(snapshot.data![index]['sender']
+                                        ['username']),
+                                    Text(snapshot.data![index]['date']),
+                                  ],
+                                ),
+                                const Icon(Icons.arrow_upward),
+                              ],
+                            ),
+                            onTap: () async {
+                              String imageUrl = await takeImage();
+                              sendImage(imageUrl,
+                                  snapshot.data![index]['sender']['uid']);
+                              await ref
+                                  .child('inboxes')
+                                  .child(user['uid'])
+                                  .child(snapshot.data![index]['sender']['uid'])
+                                  .child('replied_at')
+                                  .set(DateTime.now().toUtc().toString());
+                            },
+                          );
+                        } else {
+                          return InkWell(
+                            child: Row(
+                              children: [
+                                Image.network(
+                                  snapshot.data![index]['sender']
+                                      ['profile_picture'],
+                                  scale: 10,
+                                ),
+                                Column(
+                                  children: [
+                                    Text(snapshot.data![index]['sender']
+                                        ['username']),
+                                    Text(snapshot.data![index]['date']),
+                                  ],
+                                ),
+                                const Icon(Icons.arrow_downward),
+                              ],
+                            ),
+                            onTap: () async {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => WatchImageScreen(
+                                    imageUrl: snapshot.data![index]['image'],
+                                  ),
+                                ),
+                              );
+                              await ref
+                                  .child('inboxes')
+                                  .child(user['uid'])
+                                  .child(snapshot.data![index]['sender']['uid'])
+                                  .child('viewed')
+                                  .set(true);
+                            },
+                          );
+                        }
+                      },
                     ),
-                    Column(
-                      children: [
-                        Text(snapshot.data![index]['sender']['username']),
-                        Text(
-                          'Hai risposto ' + snapshot.data![index]['replied_at'],
-                        ),
-                      ],
-                    ),
-                    const Icon(Icons.arrow_upward),
-                  ],
-                );
-              } else if (snapshot.data[index]['viewed']) {
-                return InkWell(
-                  child: Row(
-                    children: [
-                      Image.network(
-                        snapshot.data![index]['sender']['profile_picture'],
-                        scale: 10,
-                      ),
-                      Column(
-                        children: [
-                          Text(snapshot.data![index]['sender']['username']),
-                          Text(snapshot.data![index]['date']),
-                        ],
-                      ),
-                      const Icon(Icons.arrow_upward),
-                    ],
-                  ),
-                  onTap: () async {
-                    String imageUrl = await takeImage();
-                    sendImage(imageUrl, snapshot.data![index]['sender']['uid']);
-                    await ref
-                        .child('inboxes')
-                        .child(user['uid'])
-                        .child(snapshot.data![index]['sender']['uid'])
-                        .child('replied_at')
-                        .set(DateTime.now().toUtc().toString());
-                  },
-                );
-              } else {
-                return InkWell(
-                  child: Row(
-                    children: [
-                      Image.network(
-                        snapshot.data![index]['sender']['profile_picture'],
-                        scale: 10,
-                      ),
-                      Column(
-                        children: [
-                          Text(snapshot.data![index]['sender']['username']),
-                          Text(snapshot.data![index]['date']),
-                        ],
-                      ),
-                      const Icon(Icons.arrow_downward),
-                    ],
-                  ),
-                  onTap: () async {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => WatchImageScreen(
-                          imageUrl: snapshot.data![index]['image'],
-                        ),
-                      ),
-                    );
-                    await ref
-                        .child('inboxes')
-                        .child(user['uid'])
-                        .child(snapshot.data![index]['sender']['uid'])
-                        .child('viewed')
-                        .set(true);
-                  },
-                );
-              }
-            },
-          );
-        }
+        );
       },
     );
   }
