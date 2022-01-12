215652f0c760a7ceae2d1f2307f75f6c13d91841
Filippo Valli <filippovalli2003@gmail.com>
2022-01-12 10:20:03 +0100

    create fetchInbox() provider

---
+++ lib/providers/fetch_inbox.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:sn/providers/sign_up.dart';
+
+Future<List> fetchInbox() async {
+  var inbox;
+  List inboxList = [];
+
+  // instatiate reference of Realtime Database
+  final DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+  // create or update user in Realtime Database
+  await ref
+      .child('users')
+      .child(user['uid'])
+      .child('inbox')
+      .get()
+      .then((value) {
+    inbox = value.value;
+  });
+
+  await inbox.forEach((key, value) => inboxList.add(value));
+
+  return inboxList;
+}
