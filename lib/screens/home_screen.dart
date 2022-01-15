809b238204d28fd4c7a3ac91686c7f3374be908e
Filippo Valli <filippovalli2003@gmail.com>
2022-01-15 18:19:30 +0100

    add refresh to HomeScreen()

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,14 @@ import 'package:sn/widgets/start_chat_button.dart';
 import 'package:sn/widgets/user_has_to_reply_card.dart';
 import 'package:sn/widgets/user_replied_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,32 +28,38 @@ class HomeScreen extends StatelessWidget {
             return (snapshot.connectionState == ConnectionState.waiting)
                 // snapshot is waiting
                 ? const Center(child: CircularProgressIndicator())
-                : ListView(
-                    children: [
-                      const StartChatButton(),
-                      (snapshot.hasError)
-                          // snapshot has error
-                          ? Text(snapshot.error.toString())
-                          // snapshot has data
-                          : ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: snapshot.data.length,
-                              itemBuilder: (BuildContext context, int index) {
-                                if (snapshot.data![index]['sender'] ==
-                                    user['uid']) {
-                                  // user replied last
-                                  return UserRepliedCard(
-                                    chatData: snapshot.data![index],
-                                  );
-                                } else {
-                                  // user has to reply
-                                  return UserHasToReplyCard(
-                                    chatData: snapshot.data![index],
-                                  );
-                                }
-                              },
-                            ),
-                    ],
+                : RefreshIndicator(
+                    onRefresh: () async {
+                      await fetchChats();
+                      setState(() {});
+                    },
+                    child: ListView(
+                      children: [
+                        const StartChatButton(),
+                        (snapshot.hasError)
+                            // snapshot has error
+                            ? Text(snapshot.error.toString())
+                            // snapshot has data
+                            : ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  if (snapshot.data![index]['sender'] ==
+                                      user['uid']) {
+                                    // user replied last
+                                    return UserRepliedCard(
+                                      chatData: snapshot.data![index],
+                                    );
+                                  } else {
+                                    // user has to reply
+                                    return UserHasToReplyCard(
+                                      chatData: snapshot.data![index],
+                                    );
+                                  }
+                                },
+                              ),
+                      ],
+                    ),
                   );
           },
         ),
