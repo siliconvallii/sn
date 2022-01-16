c2b5beb243f0d2df57af72705db00ef55a9a68e1
Filippo Valli <filippovalli2003@gmail.com>
2022-01-16 11:42:30 +0100

    fetchChats() now updates friendship status

--- lib/providers/fetch_chats.dart
+++ lib/providers/fetch_chats.dart
@@ -8,14 +8,36 @@ Future<List> fetchChats() async {
   // fetch chats
   List chats = [];
 
-  await ref.child('chats').get().then((snapshot) {
+  await ref.child('chats').get().then((snapshot) async {
     // fetch all chats
     dynamic _allChatsMap = snapshot.value;
 
-    _allChatsMap.forEach((key, value) {
+    await _allChatsMap.forEach((key, value) async {
       // check if user is involved in each chat
       if (key.contains(user['uid'])) {
-        // user is involved, add chat to chatsMap
+        // user is involved
+
+        // update friendship status
+
+        // check if users are in touch
+        if (DateTime.now()
+                .difference(
+                  DateTime.parse(value['sent_at']),
+                )
+                .inHours <
+            24) {
+          // users are friends
+
+          // update friendship status to true
+          await ref.child('chats').child(key).child('friends').set(true);
+        } else {
+          // user aren't friends
+
+          // update friendship status to false
+          await ref.child('chats').child(key).child('friends').set(false);
+        }
+
+        // add chat to chatsMap
         chats.add(value);
       }
     });
