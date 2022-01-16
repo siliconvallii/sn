f2d9c2a6f5991b26196c7cbc6e769c54ec8e9ddd
Filippo Valli <filippovalli2003@gmail.com>
2022-01-16 13:10:10 +0100

    fix MyProfileScreen()

--- lib/screens/my_profile_screen.dart
+++ lib/screens/my_profile_screen.dart
@@ -13,6 +13,36 @@ class MyProfileScreen extends StatelessWidget {
           profile['username'],
         ),
       ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SizedBox(
+                    child: Image.network(
+                      profile['profile_picture'],
+                    ),
+                    height: 100,
+                  ),
+                  Column(
+                    children: [
+                      Text('${profile['total_friends']} amici'),
+                      ElevatedButton(
+                        child: const Text('Modifica profilo'),
+                        onPressed: () {},
+                      ),
+                    ],
+                  )
+                ],
+              ),
+              Text(profile['username']),
+              Text('Liceo Sarpi - ${profile['class']}${profile['section']}'),
+              Text(profile['bio']),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
