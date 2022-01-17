6ece9f5b5bdfc7f48b31a956b7d80b6d06dab118
Filippo Valli <filippovalli2003@gmail.com>
2022-01-17 18:25:56 +0100

    create fetchRandomUser()

---
+++ lib/providers/fetch_random_user.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:sn/providers/sign_in.dart';
+
+Future<Map> fetchRandomUser() async {
+  // instatiate reference of Realtime Database
+  final DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+  dynamic users;
+  Map randomUser = {};
+
+  await ref.child('users').get().then((snapshot) {
+    users = snapshot.value;
+
+    List usersList = [];
+    users.forEach((key, value) => usersList.add(value));
+
+    while (randomUser['uid'] == null ||
+        randomUser['uid'] == user['uid'] ||
+        randomUser['friends'].contains(user['uid'])) {
+      randomUser = (usersList.toList()..shuffle()).first;
+    }
+  });
+
+  return randomUser;
+}
