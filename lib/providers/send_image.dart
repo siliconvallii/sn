88e470dc60b6a78e3f443853668eb84071fdfb3d
Filippo Valli <filippovalli2003@gmail.com>
2022-01-17 18:13:42 +0100

    improve sendImage()

--- lib/providers/send_image.dart
+++ lib/providers/send_image.dart
@@ -1,55 +1,17 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:sn/providers/sign_in.dart';
 
-dynamic users;
-
-Future<Map> getRandomUser() async {
-  // instatiate reference of Realtime Database
-  final DatabaseReference ref = FirebaseDatabase.instance.ref();
-
-  Map randomUser = {};
-
-  await ref.get().then((snapshot) {
-    users = snapshot.child('users').value;
-
-    List usersList = [];
-    users.forEach((key, value) => usersList.add(value));
-
-    randomUser = (usersList.toList()..shuffle()).first;
-  });
-
-  return randomUser;
-}
-
 void sendImage(String imageUrl, String recipient) async {
   // instatiate reference of Realtime Database
   final DatabaseReference ref = FirebaseDatabase.instance.ref();
 
-  if (recipient == 'random') {
-    // get randomUser
-    Map randomUser = await getRandomUser();
-    while (randomUser['uid'] == user['uid']) {
-      randomUser = await getRandomUser();
-    }
-
-    // put image in randomUser's inbox
-    ref.child('inboxes').child(randomUser['uid']).child(user['uid']).set(
-      {
-        'image': imageUrl,
-        'date': DateTime.now().toUtc().toString(),
-        'sender': user,
-        'viewed': false,
-      },
-    );
-  } else {
-    // put image in recipient's inbox
-    ref.child('inboxes').child(recipient).child(user['uid']).set(
-      {
-        'image': imageUrl,
-        'date': DateTime.now().toUtc().toString(),
-        'sender': user,
-        'viewed': false,
-      },
-    );
-  }
+  // put image in recipient's inbox
+  await ref.child('inboxes').child(recipient).child(user['uid']).set(
+    {
+      'image': imageUrl,
+      'date': DateTime.now().toUtc().toString(),
+      'sender': user,
+      'viewed': false,
+    },
+  );
 }
