781d6f5edb70c5cd842b47c25993d75a93623f34
Filippo Valli <filippovalli2003@gmail.com>
2022-01-17 08:49:54 +0100

    fix StartChatButton() styling

--- lib/widgets/start_chat_button.dart
+++ lib/widgets/start_chat_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartChatButton extends StatelessWidget {
   const StartChatButton({Key? key}) : super(key: key);
@@ -6,10 +7,19 @@ class StartChatButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      child: const Text('Avvia una nuova chat'),
-      onPressed: () {
-        Navigator.pushNamed(context, '/new_chat');
-      },
+      child: const Text('inizia una nuova chat'),
+      onPressed: () => Navigator.pushNamed(context, '/new_chat'),
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(
+          const Color(0xff63D7C6),
+        ),
+        foregroundColor: MaterialStateProperty.all(const Color(0xff121212)),
+        textStyle: MaterialStateProperty.all(
+          GoogleFonts.alata(
+            fontSize: 14,
+          ),
+        ),
+      ),
     );
   }
 }
